42cd68e21b8fea9ed228d5726e8d80aca3e6d0c0
Bartosz Wróblewski <bawr@hszm.pl>
2021-11-29 10:19:47 -0800

    EGL: take out debugging code.

--- platform/egl/os_egl.cpp
+++ platform/egl/os_egl.cpp
@@ -164,8 +164,6 @@ Error OS_EGL::initialize(const VideoMode &p_desired, int p_video_driver, int p_a
 
 #endif
 
-	ERR_PRINT("GLES3 selected, but we're about to crash... ;_;")
-
 	visual_server = memnew(VisualServerRaster);
 	if (get_render_thread_mode() != RENDER_THREAD_UNSAFE) {
 		visual_server = memnew(VisualServerWrapMT(visual_server, get_render_thread_mode() == RENDER_SEPARATE_THREAD));
