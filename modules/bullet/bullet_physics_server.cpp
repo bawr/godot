6fed48b705d64af62c81c55df4eabad8c66018c0
Bartosz Wróblewski <bawr@hszm.pl>
2022-04-04 19:24:14 -0700

    Revert "Revert "Move Bullet physics query flush...""
    This reverts commit 10544f1cf777324efc676565ecc698f8ba9f06e3.

--- modules/bullet/bullet_physics_server.cpp
+++ modules/bullet/bullet_physics_server.cpp
@@ -1549,6 +1549,12 @@ void BulletPhysicsServer::step(float p_deltaTime) {
 }
 
 void BulletPhysicsServer::flush_queries() {
+	if (!active)
+		return;
+
+	for (int i = 0; i < active_spaces_count; ++i) {
+		active_spaces[i]->flush_queries();
+	}
 }
 
 void BulletPhysicsServer::finish() {
